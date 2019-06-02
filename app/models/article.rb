4027cc6c7c32b20470f94e075989c70549f4b70d
Nhung Hoang <nhung@nhunghoang.com>
2019-06-02 13:48:11 +1000

    added model tag

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_and_belongs_to_many :tags
   validates :title, presence: :true,
                     length: { minimum: 5 }
 end
